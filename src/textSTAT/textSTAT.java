0b951f4a8cd559ef388cf1ddcf09cc2636f4427b
dlsrnsi <dlsrnsladlek@naver.com>
2014-05-26 20:32:15 +0900

    let's Start!

---
+++ src/textSTAT/textSTAT.java
@@ -0,0 +1,30 @@
+package textSTAT;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class textSTAT extends Application{
+
+	 public static void main(String[] args) {
+	        Application.launch(textSTAT.class, (java.lang.String[])null);
+	    }
+
+	    @Override
+	    public void start(Stage primaryStage) {
+	        try {
+	            AnchorPane page = (AnchorPane) FXMLLoader.load(textSTAT.class.getResource("MainGUI.fxml"));
+	            Scene scene = new Scene(page);
+	            primaryStage.setScene(scene);
+	            primaryStage.setTitle("textSTAT");
+	            primaryStage.show();
+	        } catch (Exception ex) {
+	            Logger.getLogger(textSTAT.class.getName()).log(Level.SEVERE, null, ex);
+	        }
+	    }
+}
